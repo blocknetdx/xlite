ee1f6677746fb9af6cfac330c68e7bd3df8f6863
M <magic.ru789@gmail.com>
2020-08-07 01:09:27 -0500

    Default app size 16:9

--- src/app/windows/app-window.js
+++ src/app/windows/app-window.js
@@ -5,14 +5,17 @@ import BrowserWindow from '../modules/browser-window';
 import { storageKeys } from '../constants';
 
 const openAppWindow = storage => {
-  const { height, width } = electron.screen.getPrimaryDisplay().workAreaSize;
+  let { height, width } = electron.screen.getPrimaryDisplay().workAreaSize;
+  width *= 0.6;
+  const nheight = width * 9/16;
+  height = height >= nheight ? nheight : height;
   return new BrowserWindow({
     filePath: path.resolve(__dirname, '../../index.html'),
     toggleDevTools: isDev ? true : false,
     isMainWindow: true,
     windowOptions: {
-      width: width - 300,
-      height: height - 200
+      width: width,
+      height: height
     },
     webPreferences: {
       zoomFactor: 1.0
