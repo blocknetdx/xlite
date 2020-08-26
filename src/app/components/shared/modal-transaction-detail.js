67dc7826486d395deec9a29d29bcb1e9f24afb90
M <magic.ru789@gmail.com>
2020-08-25 19:51:56 -0500

    Improve modal tx detail data provider

--- src/app/components/shared/modal-transaction-detail.js
+++ src/app/components/shared/modal-transaction-detail.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Modal, ModalBody, ModalHeader } from './modal';
 import Localize from './localize';
 import { Column, Row } from './flex';
+import {currencyLinter, multiplierForCurrency} from '../../util';
 import path from 'path';
 import { IMAGE_DIR } from '../../constants';
 import { all, create } from 'mathjs';
@@ -19,17 +20,17 @@ const Divider = () => <div style={{flexGrow: 1, height: 1, backgroundColor: 'rgb
 const TransactionDetailModal = ({ altCurrency, currencyMultipliers, selectedTx, onClose }) => {
 
   const { ticker } = selectedTx.wallet;
-  const selectedAltMultiplier = bignumber(currencyMultipliers[ticker] && currencyMultipliers[ticker][altCurrency] ? currencyMultipliers[ticker][altCurrency] : 0);
-  const selectedBTCMultiplier = bignumber(currencyMultipliers[ticker] && currencyMultipliers[ticker]['BTC'] ? currencyMultipliers[ticker]['BTC'] : 0);
+  const selectedAltMultiplier = multiplierForCurrency(ticker, altCurrency, currencyMultipliers);
+  const selectedBTCMultiplier = multiplierForCurrency(ticker, 'BTC', currencyMultipliers);
 
   const onViewOnExplorer = e => {
     e.preventDefault();
-    electron.shell.openExternal(`https://live.blockcypher.com/${selectedTx.wallet.ticker.toLowerCase()}/tx/${selectedTx.txId}`);
+    electron.shell.openExternal(selectedTx.wallet.getExplorerLinkForTx(selectedTx.tx.txId));
   };
 
   return (
     <Modal onClose={onClose}>
-      <ModalHeader>{selectedTx.type === 'send' ?
+      <ModalHeader>{selectedTx.tx.isSend() ?
         Localize.text('Sent {{coin}}', 'transactions', {coin: selectedTx.wallet.ticker})
         :
         Localize.text('Received {{coin}}', 'transactions', {coin: selectedTx.wallet.ticker})
@@ -37,7 +38,7 @@ const TransactionDetailModal = ({ altCurrency, currencyMultipliers, selectedTx, 
       <ModalBody className={'lw-modal-transaction-body'}>
         <Row justify={'center'}>
           <img alt={Localize.text('Transaction icon', 'transactions')}
-               srcSet={selectedTx.type === 'send' ?
+               srcSet={selectedTx.tx.isSend() ?
                  `${path.join(IMAGE_DIR, 'icons', 'icon-sent-large.png')}, ${path.join(IMAGE_DIR, 'icons', 'icon-sent-large@2x.png')} 2x, ${path.join(IMAGE_DIR, 'icons', 'icon-sent-large@3x.png')} 3x`
                  :
                  `${path.join(IMAGE_DIR, 'icons', 'icon-received-large.png')}, ${path.join(IMAGE_DIR, 'icons', 'icon-received-large@2x.png')} 2x, ${path.join(IMAGE_DIR, 'icons', 'icon-received-large@3x.png')} 3x`
@@ -46,15 +47,15 @@ const TransactionDetailModal = ({ altCurrency, currencyMultipliers, selectedTx, 
         </Row>
         <Row justify={'center'}>
           <div className={'lw-modal-transaction-total-container'}>
-                <span className={'lw-color-secondary-6'}>{selectedTx.type === 'send' ?
+                <span className={'lw-color-secondary-6'}>{selectedTx.tx.isSend() ?
                   Localize.text('Total sent', 'transactions')
                   :
                   Localize.text('Total Received', 'transactions')
-                }</span>: <span className={'lw-color-secondary-10'}><span className={'text-monospace'}>{selectedTx.amount}</span> {selectedTx.wallet.ticker}</span>
+                }</span>: <span className={'lw-color-secondary-10'}><span className={'text-monospace'}>{selectedTx.tx.amount}</span> {selectedTx.wallet.ticker}</span>
           </div>
         </Row>
         <Row justify={'center'}>
-          <div style={{color: '#777', marginBottom: 36}}>{altCurrency} <span className={'text-monospace'}>{math.multiply(bignumber(selectedTx.amount), selectedAltMultiplier).toFixed(2)}</span></div>
+          <div style={{color: '#777', marginBottom: 36}}>{altCurrency} <span className={'text-monospace'}>{currencyLinter(math.multiply(bignumber(selectedTx.tx.amount), bignumber(selectedAltMultiplier)))}</span></div>
         </Row>
         <Row>
           <Divider />
@@ -65,8 +66,8 @@ const TransactionDetailModal = ({ altCurrency, currencyMultipliers, selectedTx, 
           </Column>
           <Column justify={'center'}>
             <div>
-              <div className={'lw-color-secondary-10'}>BTC <span className={'text-monospace'}>{selectedBTCMultiplier.toNumber()}</span></div>
-              <div className={'lw-color-secondary-6'}>{altCurrency} <span className={'text-monospace'}>{selectedAltMultiplier.toNumber()}</span></div>
+              <div className={'lw-color-secondary-10'}>BTC <span className={'text-monospace'}>{selectedBTCMultiplier}</span></div>
+              <div className={'lw-color-secondary-6'}>{altCurrency} <span className={'text-monospace'}>{selectedAltMultiplier}</span></div>
             </div>
           </Column>
         </Row>
@@ -74,8 +75,8 @@ const TransactionDetailModal = ({ altCurrency, currencyMultipliers, selectedTx, 
           <Divider />
         </Row>
         <Row justify={'space-between'} style={{fontSize: 14, paddingTop: 16, paddingBottom: 16}}>
-          <div className={'lw-color-secondary-6'}>{selectedTx.type === 'send' ? Localize.text('To address', 'transactions') : Localize.text('From address', 'transactions')}:</div>
-          <div className={'lw-color-secondary-10 text-monospace'}>{selectedTx.address}</div>
+          <div className={'lw-color-secondary-6'}>{selectedTx.tx.isSend() ? Localize.text('To address', 'transactions') : Localize.text('From address', 'transactions')}:</div>
+          <div className={'lw-color-secondary-10 text-monospace'}>{selectedTx.tx.address}</div>
         </Row>
         <Row>
           <Divider />
@@ -91,7 +92,7 @@ const TransactionDetailModal = ({ altCurrency, currencyMultipliers, selectedTx, 
 TransactionDetailModal.propTypes = {
   altCurrency: PropTypes.string,
   currencyMultipliers: PropTypes.object,
-  selectedTx: PropTypes.object,
+  selectedTx: PropTypes.object, // { tx: RPCTransaction, wallet: Wallet }
   onClose: PropTypes.func
 };
 
