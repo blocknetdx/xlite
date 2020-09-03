60e35c91d827a918de58702d23f2669fc254c541
Ryan Burgett <ryan@burgettdev.net>
2020-09-03 02:44:50 -0400

    Added backup modal UI

--- src/app/components/shared/modal-backup.js
+++ src/app/components/shared/modal-backup.js
@@ -4,13 +4,41 @@ import React from 'react';
 import { Modal, ModalBody, ModalHeader } from './modal';
 import Localize from './localize';
 import * as appActions from '../../actions/app-actions';
+import { Button } from './buttons';
 
 const BackupModal = ({ hideBackupModal }) => {
 
+  const onDownloadFileClick = e => {
+    e.preventDefault();
+
+    // ToDo add download backup file functionality
+
+  };
+
+  const styles = {
+    body: {
+      paddingLeft: 20,
+      paddingRight: 20
+    },
+    spacer: {
+      height: 200
+    },
+    buttonContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginBottom: 40
+    }
+  };
+
   return (
     <Modal onClose={hideBackupModal}>
-      <ModalHeader><Localize context={'receive-modal'}>Backup</Localize></ModalHeader>
-      <ModalBody>
+      <ModalHeader><Localize context={'backup-modal'}>Backup</Localize></ModalHeader>
+      <ModalBody style={styles.body}>
+        <p><Localize context={'backup-modal'}>The following downloaded file can be used to recover your funds. Make sure to download and store it in a safe and secure place.</Localize></p>
+        <p><strong><Localize context={'backup-modal'}>Do not share this file with anyone as it provides access to your wallet and therefore your funds.</Localize></strong></p>
+        <div style={styles.spacer} />
+        <div style={styles.buttonContainer}><Button onClick={onDownloadFileClick}><Localize context={'backup-modal'}>Download backup file</Localize> <i className={'fas fa-download'} /></Button></div>
       </ModalBody>
     </Modal>
   );
