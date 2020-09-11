895c5718a44cdab9cefd00e9cb08617325adba3f
Mirko Justiniano <mirkoj@gmail.com>
2020-09-11 16:59:48 -0500

    Implemented refresh transactions functionality

--- src/app/components/shared/transactions-panel-header.js
+++ src/app/components/shared/transactions-panel-header.js
@@ -1,15 +1,23 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
+import { connect, useStore } from 'react-redux';
+import * as appActions from '../../actions/app-actions';
+import WalletController from '../../modules/wallet-controller-r';
 import PanelFilters from './button-filters';
 import { SquareButton } from './buttons';
 import { transactionFilters } from '../../constants';
 
-const TransactionsPanelHeader = ({ selectedFilter, onTransactionFilter }) => {
+let TransactionsPanelHeader = ({ selectedFilter, onTransactionFilter, walletController }) => {
 
+  const store = useStore();
   const [transactionFilter, setTransactionFilter] = useState(selectedFilter || transactionFilters.all);
 
-  const onReloadButton = () => {
-    // ToDo: Implement reload transactions action
+  const onRefreshButton = () => {
+    walletController.updateAllBalances()
+      .then(() => {
+        walletController.dispatchBalances(appActions.setBalances, store);
+        walletController.dispatchTransactions(appActions.setTransactions, store);
+      });
   };
 
   const onFilterButton = () => {
@@ -20,7 +28,7 @@ const TransactionsPanelHeader = ({ selectedFilter, onTransactionFilter }) => {
     <div className={'lw-transactions-panel-header'}>
       <div className={'lw-transactions-panel-header-title'}>
         <h1>Latest Transactions</h1>
-        <i className={'fas fa-undo fa-flip-horizontal'} onClick={onReloadButton} />
+        <i className={'fas fa-undo fa-flip-horizontal'} onClick={onRefreshButton} />
       </div>
       <div className={'lw-transactions-panel-header-filters'}>
         <div className={'lw-transactions-panel-text'}>Show:</div>
@@ -37,7 +45,14 @@ const TransactionsPanelHeader = ({ selectedFilter, onTransactionFilter }) => {
 
 TransactionsPanelHeader.propTypes = {
   selectedFilter: PropTypes.string,
-  onTransactionFilter: PropTypes.func
+  onTransactionFilter: PropTypes.func,
+  walletController: PropTypes.instanceOf(WalletController)
 };
+
+TransactionsPanelHeader = connect(
+  ({ appState }) => ({
+    walletController: appState.walletController,
+  })
+)(TransactionsPanelHeader);
 
 export default TransactionsPanelHeader;
