5096b633de135e33607a0070651110a85addfb41
tryiou <73646876+tryiou@users.noreply.github.com>
2023-06-23 14:47:57 +0200

    Update logger.js

--- src/server/modules/logger.js
+++ src/server/modules/logger.js
@@ -1,25 +1,28 @@
 // Copyright (c) 2020 The Blocknet developers
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
-import { DATA_DIR } from '../constants';
-import { createLogger, format, transports } from 'winston';
+import {DATA_DIR} from '../constants';
+
+import {createLogger, format, transports} from 'winston';
 import isDev from 'electron-is-dev';
 import path from 'path';
 
 // A Winston logger instance for logging errors and other info
 export const logger = createLogger({
-  format: format.combine(format.timestamp(), format.json()),
+  format: format.combine(
+    format.timestamp(),
+    format.simple(),
+  ),
   transports: [
     new transports.File({
-      filename: path.join(DATA_DIR, /*isRenderer() ? 'renderer.log' : */ 'main.log'),
+      filename: path.join(DATA_DIR, /*isRenderer() ? 'renderer.log' : */'main.log'),
       maxsize: 1000000,
       maxFiles: 5,
-      tailable: true,
-      level: 'debug', // Set the log level to 'debug'
-    }),
-  ],
+      tailable: true
+    })
+  ]
 });
 
 if (isDev) {
-  logger.add(new transports.Console({ level: 'debug' })); // Set the log level to 'debug'
+  logger.add(new transports.Console());
 }
